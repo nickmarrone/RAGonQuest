c8150512a8c6c07a91edf19a8d543f9ea4160c66
Nicholas Marrone <nickmarrone@gmail.com>
2025-07-05 10:06:39 -0700

    Adjust CSS so main window isn't scrolled over

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,15 +6,17 @@ import ConversationView from './components/ConversationView';
 function App() {
 
   return (
-    <div className="flex h-screen bg-zinc-900 text-white">
+    <div className="flex h-screen bg-zinc-900 text-white overflow-hidden">
       {/* Sidebar */}
-      <div className="w-64 min-w-[200px] bg-zinc-800 border-r border-zinc-700 p-4 flex flex-col">
+      <div className="w-64 min-w-[200px] bg-zinc-800 border-r border-zinc-700 p-4 flex flex-col overflow-y-auto">
         <Corpora />
         <ConversationsList />
       </div>
       {/* Main Content */}
-      <div className="flex-1 p-10 overflow-y-auto">
-        <ConversationView />
+      <div className="flex-1 flex flex-col overflow-hidden">
+        <div className="flex-1 p-6 overflow-y-auto">
+          <ConversationView />
+        </div>
       </div>
     </div>
   );
